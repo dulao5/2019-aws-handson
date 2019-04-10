2e44d6740731bf57f0d9be5881e34ae4b713a58f
dzg <dulao5@gmail.com>
2019-04-11 08:26:52 +0900

    use named-pipe for logging stream

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 set -x \
     && mkdir -p /var/log/nginx \
-    && chown nginx:appgroup /var/log/nginx \
-    && chmod g+s /var/log/nginx
+    && mkfifo /var/log/nginx/site_error.log /var/log/nginx/site_access.log \
+    && chown -R nginx:appgroup /var/log/nginx \
+    && chmod -R g+s /var/log/nginx \
+    && chmod g+r /var/log/nginx/site_error.log /var/log/nginx/site_access.log \
 
 set -x \
     && sed "s/\${PHP_HOST}/${PHP_HOST}/" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf \
